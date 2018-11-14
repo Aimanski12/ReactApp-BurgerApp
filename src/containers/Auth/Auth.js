418d2beb2e35e5e4d2b6423995ba5bc503786b7b
Leo espinoza <leo.spinoza@yahoo.com>
2018-11-13 23:54:47 -0600

    added the authentication

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
 
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
 
 import classes from './Auth.css'
+
+import * as actions from '../../store/actions/index'
+
+
 class Auth extends Component {
 
   state = {
@@ -85,6 +90,11 @@ inputChangedHandler = (event, controlName) => {
   this.setState({controls: updatedControls})
 }
 
+submitHandler = (event) => {
+  event.preventDefault();
+  this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value)
+}
+
   render () {
 
     const formElementsArray = [];
@@ -110,7 +120,7 @@ inputChangedHandler = (event, controlName) => {
       
       return (
         <div className={classes.Auth}>
-          <form action="">
+          <form onSubmit={this.submitHandler}>
             {form}
             <Button btnType="Success" >Submit</Button>
           </form>
@@ -119,4 +129,13 @@ inputChangedHandler = (event, controlName) => {
   }
 }
 
-export default Auth
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onAuth: (email, password) => {
+      dispatch(actions.auth(email, password))
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Auth)
